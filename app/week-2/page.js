ccf0bb24bafc8be2aad8029fa969bc107201490f
jackslemp <Jackson.slemp@edu.sait.ca>
2025-03-04 12:12:20 -0700

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StudentInfo from "./student-info";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Week 2 - Student Information</h1>
+      <StudentInfo />
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Jack Slemp</h1>
+      <p>
+        GitHub Repository: 
+        <a href="https://github.com/jackslemp/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+          Click here
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
